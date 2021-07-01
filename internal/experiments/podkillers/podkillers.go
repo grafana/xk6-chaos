2791d98e25a4aca12bd78eb6925748f6ed2f43ae
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2021-07-01 12:41:17 +0200

    Added KillByKeyword function to pods and KillPodLike to podkillers

--- internal/experiments/podkillers/podkillers.go
+++ internal/experiments/podkillers/podkillers.go
@@ -1,18 +1,16 @@
 package podkillers
 
 import (
-    "context"
-    
+	"context"
+
 	"github.com/simskij/xk6-chaos/internal/k8s/pods"
-    "github.com/simskij/xk6-chaos/pkg/k8s/client"
+	"github.com/simskij/xk6-chaos/pkg/k8s/client"
 )
-
-
 
 // This exposes podkiller metadata for use in displaying results.
 type Podkillers struct {
-    Ready bool
-    pod *pods.Pods
+	Ready bool
+	pod   *pods.Pods
 }
 
 var victims []string = []string{}
@@ -20,37 +18,44 @@ var numOfPods int
 
 // New creates a new podkiller
 func New(Ready bool) *Podkillers {
-    c, err := client.New()
-    p := pods.New(c)
-    if err !=nil {
-        return nil
-    }
+	c, err := client.New()
+	p := pods.New(c)
+	if err != nil {
+		return nil
+	}
 	return &Podkillers{Ready, p}
 }
 
 // AddVictim saves the names of pods selected to be terminated.
 func (p *Podkillers) AddVictim(victim string) {
-    victims = append(victims, victim)
+	victims = append(victims, victim)
 }
 
 // GetVictims returns the string array of all pods selected to be terminated.
 func (p *Podkillers) GetVictims() []string {
-    return victims
+	return victims
 }
 
 // SetStartingPods saves the number of pods at the beginning of a test as the "before" state.
 func (p *Podkillers) SetStartingPods(number int) {
-    numOfPods = number
+	numOfPods = number
 }
 
 // GetStartingPods returns the number of pods at the beginning of the test for reporting purposes.
 func (p *Podkillers) GetStartingPods() int {
-    return numOfPods
+	return numOfPods
 }
 
 // KillPod terminates a k8s pod identified by name.
 func (p *Podkillers) KillPod(namespace string, podName string) error {
-    ctx := context.Background()
-    err := p.pod.KillByName(ctx, namespace, podName)
-    return err
-} 
+	ctx := context.Background()
+	err := p.pod.KillByName(ctx, namespace, podName)
+	return err
+}
+
+// KillPodLike terminates a k8s pod whose name contains string.
+func (p *Podkillers) KillPodLike(namespace string, keyword string) error {
+	ctx := context.Background()
+	err := p.pod.KillByKeyword(ctx, namespace, keyword)
+	return err
+}
--- internal/k8s/pods/pods.go
+++ internal/k8s/pods/pods.go
@@ -2,6 +2,8 @@ package pods
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	coreV1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -41,6 +43,23 @@ func (pods *Pods) KillByName(ctx context.Context, namespace string, podName stri
 	podsInNamespace := pods.client.CoreV1().Pods(namespace)
 	err := podsInNamespace.Delete(ctx, podName, v1.DeleteOptions{})
 
+	return err
+}
+
+// KillByKeyword kills the first pod with a name that contains the specified keyword
+func (pods *Pods) KillByKeyword(ctx context.Context, namespace string, podKeyword string) error {
+	// Iterate through podnames in list and find one that matches keyword
+	var podsList, err = pods.List(ctx, namespace)
+	var podToCheck = ""
+	for i := 0; i < len(podsList); i++ {
+		podToCheck = podsList[i]
+		if strings.Contains(podToCheck, podKeyword) {
+			fmt.Println(podToCheck + " contains " + podKeyword + "and will be terminated.")
+			break
+		}
+	}
+	var podName = podToCheck
+	err = pods.KillByName(ctx, namespace, podName)
 	return err
 }
 
